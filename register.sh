d85d6f0ce3b42b50910a8e7347437068d133a468
Jon Campbell <jonathan.campbell@am.sony.com>
2016-05-23 13:28:35 -0700

    Adding de-register option to register script

--- register.sh
+++ register.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set +x
+#set +x
+set -x
 
 #$POST_USER - env must be set 
 #$POST_KEY - env must be set 
@@ -9,6 +10,7 @@ set +x
 #$SLAVE_IP - env must be set
 #$SLAVE_EXECUTORS - env must be set
 
+ACTION=${1:-register}
 SLAVE_LABELS="docker docker-${HOSTNAME} docker-latest"
 SLAVE_NAME="docker-${HOSTNAME}"
 JENKINS_HOME="${JENKINS_HOME:-/var/jenkins}"
@@ -26,6 +28,62 @@ function post_to_master () {
     --data-binary "${DATA}" \
     --user "${POST_USER}:${POST_KEY}" \
     "http://${MASTER}/scriptText/"
+
+}
+
+function add_or_remove () {
+
+  if [ "$1" == 'add' ]
+  then
+
+    ADD_NODE="
+    Jenkins.instance.addNode( 
+                   new DumbSlave(
+                         '${SLAVE_NAME}',
+                         '',
+                         '${JENKINS_HOME}',
+                         '${SLAVE_EXECUTORS}',
+                         Node.Mode.EXCLUSIVE,
+                         '${SLAVE_LABELS}',
+                         sshLauncher,
+                         new RetentionStrategy.Always(),
+                         new LinkedList() 
+                         )
+    )
+    "
+
+  else
+
+    ADD_NODE=''
+
+  fi
+}
+
+function build_data () {
+
+  DATA="script=
+    import jenkins.model.*
+    import hudson.model.*
+    import hudson.slaves.*
+    import hudson.plugins.sshslaves.SSHLauncher
+
+    sshLauncher = new SSHLauncher(
+                  '${SLAVE_IP}',
+                  ${SLAVE_PORT},
+                  '${CREDENTIALS_ID}',
+                  '',
+                  '',
+                  '',
+                  '',
+                  0,
+                  5,
+                  5
+    )
+
+    ${REMOVE_NODE}
+
+    ${ADD_NODE}
+  "
 
 }
 
@@ -54,55 +112,25 @@ for ( slave in hudson.model.Hudson.instance.slaves ) {
 
 fi
 
-DATA="script=
-import jenkins.model.*
-import hudson.model.*
-import hudson.slaves.*
-import hudson.plugins.sshslaves.SSHLauncher
-
-sshLauncher = new SSHLauncher(
-                  '${SLAVE_IP}',
-                  ${SLAVE_PORT},
-                  '${CREDENTIALS_ID}',
-                  '',
-                  '',
-                  '',
-                  '',
-                  0,
-                  5,
-                  5
-)
-
-${REMOVE_NODE}
-
-Jenkins.instance.addNode( 
-                   new DumbSlave(
-                         '${SLAVE_NAME}',
-                         '',
-                         '${JENKINS_HOME}',
-                         '${SLAVE_EXECUTORS}',
-                         Node.Mode.EXCLUSIVE,
-                         '${SLAVE_LABELS}',
-                         sshLauncher,
-                         new RetentionStrategy.Always(),
-                         new LinkedList() 
-                         )
-)
-"
-
-if [ "${SLAVE_EXISTS}" == "true" ]
+if [ "$1" == "register" ]
 then
 
-  echo -e "\n## Updating slave (${SLAVE_NAME}) on master (${MASTER}) ##"
+  add_or_remove add
+  build_data
+
+  echo -e "\n## Registering slave (${SLAVE_NAME}) with master (${MASTER}) ##"
   echo -n "Response Status Code: "
   post_to_master "true"
-  echo -e "\n## Update Complete ##\n"
+  echo -e "\n## Registration Complete ##\n"
 
 else
 
-  echo -e "\n## Adding slave (${SLAVE_NAME}) to master (${MASTER}) ##"
+  add_or_remove remove
+  build_data
+
+  echo -e "\n## De-Registering slave (${SLAVE_NAME}) with master (${MASTER}) ##"
   echo -n "Response Status Code: "
   post_to_master "true"
-  echo -e "\n## Additon Complete ##\n"
+  echo -e "\n## De-Registrataion Complete ##\n"
 
 fi
